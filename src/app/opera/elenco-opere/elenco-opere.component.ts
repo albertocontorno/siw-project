ff2ba28f91059954c7d224f207f8ad74de748620
Alberto Contorno <alberto.contorno@gmail.com>
2017-06-12 16:12:09 +0200

    commit commit commit

--- src/app/opera/elenco-opere/elenco-opere.component.ts
+++ src/app/opera/elenco-opere/elenco-opere.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Opera } from '../opera.model';
 import { OpereService } from '../opere.service';
@@ -21,6 +21,8 @@ export class ElencoOpereComponent implements OnInit {
     let h = window.innerHeight;
     let nh = $('#navbarTop').outerHeight();
     $('#nav-side').height(h-nh);
+
+    this.creaDisposizione();
   }
 
 
@@ -30,4 +32,24 @@ export class ElencoOpereComponent implements OnInit {
         if ( element ) element.scrollIntoView ( element )
       });
     }
+
+  creaDisposizione(){
+    let divWrapper = $('#div-wrapper');
+    let letteraIniziale = this.opere[0].titolo.charAt(0);
+    divWrapper.append('<h2 class="mt-5" id="'+letteraIniziale.toLocaleLowerCase()+'">'+letteraIniziale+'</h2>');
+    divWrapper.append('<hr/>');
+    let divCorr = $('<ul class="list-group"></ul>');
+    divWrapper.append(divCorr);
+    for( let opera of this.opere ){
+      if(letteraIniziale != opera.titolo[0].charAt(0)){
+        letteraIniziale = opera.titolo[0].charAt(0);
+        divWrapper.append('<h2 class="mt-5" id="'+letteraIniziale.toLocaleLowerCase()+'">'+letteraIniziale+'</h2>');
+        divWrapper.append('<hr>');
+        divCorr = $('<ul class="list-group"></ul>');
+        divWrapper.append(divCorr);
+      }
+      divCorr.append($('<li class="list-group-item">'+opera.titolo+' - '+opera.autore+'</li>'));
+    }
+
+  }
 }
--- src/app/opera/opere.service.ts
+++ src/app/opera/opere.service.ts
@@ -3,11 +3,17 @@ import { Opera } from './opera.model';
 export class OpereService{
 
   opere: Opera[] = [
+    {titolo:'A Primavera', data: new Date('15/5/1452'), id: 1, descrizione:'Un po di gente che si diverte.', autore:"Sandro Botticelli", tecnica:"olio su tela" },
+    {titolo:'A Gioconda', data: new Date('15/5/1452'), id: 1, descrizione:'Una che sorride.Male.', autore:"Leonardo da Vinci", tecnica:"olio su tela" },
+    {titolo:'Ca Primavera', data: new Date('15/5/1452'), id: 1, descrizione:'Un po di gente che si diverte.', autore:"Sandro Botticelli", tecnica:"olio su tela" },
+    {titolo:'Ca Gioconda', data: new Date('15/5/1452'), id: 1, descrizione:'Una che sorride.Male.', autore:"Leonardo da Vinci", tecnica:"olio su tela" },
     {titolo:'La Primavera', data: new Date('15/5/1452'), id: 1, descrizione:'Un po di gente che si diverte.', autore:"Sandro Botticelli", tecnica:"olio su tela" },
-    {titolo:'La Gioconda', data: new Date('15/5/1452'), id: 1, descrizione:'Una che sorride.Male.', autore:"Leonardo da Vinci", tecnica:"olio su tela" },
-    {titolo:'La Primavera', data: new Date('15/5/1452'), id: 1, descrizione:'Un po di gente che si diverte.', autore:"Sandro Botticelli", tecnica:"olio su tela" },
-    {titolo:'La Gioconda', data: new Date('15/5/1452'), id: 1, descrizione:'Una che sorride.Male.', autore:"Leonardo da Vinci", tecnica:"olio su tela" },
-    {titolo:'La Primavera', data: new Date('15/5/1452'), id: 1, descrizione:'Un po di gente che si diverte.', autore:"Sandro Botticelli", tecnica:"olio su tela" },
+    {titolo:'Gioconda', data: new Date('15/5/1452'), id: 1, descrizione:'Una che sorride.Male.', autore:"Leonardo da Vinci", tecnica:"olio su tela" },
+    {titolo:'Primavera', data: new Date('15/5/1452'), id: 1, descrizione:'Un po di gente che si diverte.', autore:"Sandro Botticelli", tecnica:"olio su tela" },
+    {titolo:'Piocondaaaa', data: new Date('15/5/1452'), id: 1, descrizione:'Una che sorride.Male.', autore:"Leonardo da Vinci", tecnica:"olio su tela" },
+    {titolo:'Grimaveraaa', data: new Date('15/5/1452'), id: 1, descrizione:'Un po di gente che si diverte.', autore:"Sandro Botticelli", tecnica:"olio su tela" },
+    {titolo:'Bioconda', data: new Date('15/5/1452'), id: 1, descrizione:'Una che sorride.Male.', autore:"Leonardo da Vinci", tecnica:"olio su tela" },
+    {titolo:'Brimavera', data: new Date('15/5/1452'), id: 1, descrizione:'Un po di gente che si diverte.', autore:"Sandro Botticelli", tecnica:"olio su tela" },
   ];
 
   getOpere(){
